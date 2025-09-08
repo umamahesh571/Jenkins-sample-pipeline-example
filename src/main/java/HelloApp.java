ab84e45b2ce95e63030f7a3829f44bc166d406fb
213narasimha <213narasimha@gmail.com>
2025-09-08 11:59:14 +0530

    Update HelloApp.java

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello from the pipeline!");
+        System.out.println("Hello from the jenkins pipeline!");
     }
 }
